dce4ba33d146b34eea6d50ff1b2b3ac67d36765b
Igor Konoval <kip_y@sfa.org.ua>
2024-01-14 19:29:03 +0200

    edit css HW-8

--- src/components/ListItem/ListItem.styled.jsx
+++ src/components/ListItem/ListItem.styled.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ListItem = styled.li`
   display: flex;
   justify-content: space-around;
+  gap: 10px;
 `;
 
 export const Button = styled.button`
@@ -13,4 +14,5 @@ export const Button = styled.button`
 export const ListItemPar = styled.p`
   font-size: 18px;
   font-weight: 500;
+  color: orangered;
 `;
